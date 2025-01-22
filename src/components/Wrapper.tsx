09fd88fd906071cea3e402e7a847a2c733a8f5a1
adrianrakiec <rakiecadrian17@gmail.com>
2025-01-22 13:46:47 +0100

    build: create navbar and wrapper components

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface WrapperProps {
+	children: React.ReactNode;
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+	return <div className='max-w-[1400px] mx-auto px-4'>{children}</div>;
+};
